f0cc9e9e89ff25e89cb822dcf70aa3615917e9a1
vikas62081 <vikas62081@gmail.com>
2022-08-27 10:23:22 +0530

    pushing debouncing changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [search, setSearch] = useState(600071);
+  const [postOffices, setPostOffices] = useState([]);
+
+  useEffect(() => {
+    const getPostOffices = setTimeout(() => {
+      fetch(`https://api.postalpincode.in/pincode/${search}`)
+        .then((resp) => resp.json())
+        .then((resp) => setPostOffices(resp[0]?.PostOffice));
+    }, 3000);
+    return () => clearTimeout(getPostOffices);
+  }, [search]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>React-App</h3>
+      <p>Debouncing in React - How to delay a JS function</p>
+      Search:{" "}
+      <input
+        type="search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {postOffices ? (
+        postOffices?.map((post, index) => <div key={index}>{post.Name}</div>)
+      ) : (
+        <p>Invalid Pincode, Please enter a valid one!</p>
+      )}
     </div>
   );
 }
